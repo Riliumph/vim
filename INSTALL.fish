5ff3b871bf4506378007a9d598f14c9f2d3cbbb3
riliumph <riliumph@gmail.com>
2020-05-17 23:34:18 +0900

    [Add] Install script for fish on macOS

---
+++ INSTALL.fish
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###
+# Convert to absolute path
+# @param $1 path
+# @return absolute path
+abs_dirname() {
+  local path="$1"
+
+  # Check path existence one by one
+  while [ -n "$path" ]; do
+    cd "${path%/*}"  # Remove the shortest pattern(/*) from right
+    local name="${path##*/}"  # Remove the longest pattern(*/) from left
+    path="$(readlink "$name" || true)"
+  done
+
+  pwd -P  # return string
+}
+
+# Make symbolic link at $HOME/.vim
+installed_path=$(abs_dirname "$0")
+ln -nfs $installed_path $HOME/.vim
+
+if [[ $? != 0 ]];then
+  exit 1
+fi
+
+# Add bash setting for vim to bashrc
+echo 'export VIM_ROOT=$HOME/.vim' >> $FISH_ROOT/config.fish
+echo 'source $VIM_ROOT/init.fish' >> $FISH_ROOT/config.fish
